08a00388d48c96c30e367450375bd6598cccf4e3
PaulNuffer <78831963+PaulNuffer@users.noreply.github.com>
2021-03-24 20:21:23 -0600

    Update SpellCheckerTester.java

--- SpellCheckerTester.java
+++ SpellCheckerTester.java
@@ -1,14 +1,17 @@
 package assign08;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SpellCheckerTester {
 	SpellChecker spellChecker;
+	File doc = new File("src/assign08/good_luck.txt");
 	
 	@BeforeEach
 	void setUp() throws Exception {
@@ -18,7 +21,30 @@ public class SpellCheckerTester {
 // addToDictionary()
 	@Test
 	void addWordToDictionary() {
+		
+		List<String> misspelledWords = spellChecker.spellCheck(doc);
+		assertTrue(misspelledWords.contains("cs"));
+		
 		spellChecker.addToDictionary("cs");
-		assertTrue(spellChecker.spellCheck("cs"));
+		
+		misspelledWords = spellChecker.spellCheck(doc);
+		assertFalse(misspelledWords.contains("cs"));
+	}
+	
+// buildDictionary()
+	@Test
+	void buildDictionary() {
+		List<String> misspelledWords = spellChecker.spellCheck(doc);
+		assertTrue(misspelledWords.contains("cs"));
+		assertTrue(misspelledWords.contains("bst"));
+		
+		ArrayList<String> words = new ArrayList<>();
+		words.add("cs");
+		words.add("bst");
+		SpellChecker testChecker = new SpellChecker(words);
+		
+		misspelledWords = testChecker.spellCheck(doc);
+		assertFalse(misspelledWords.contains("cs"));
+		assertFalse(misspelledWords.contains("bst"));
 	}
 }
